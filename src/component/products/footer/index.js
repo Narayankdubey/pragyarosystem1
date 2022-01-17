a4dbc3f71d7f867f744186bb9541b0e7c97d5009
Narayan Dubey <narayan.d@valuebound.com>
2022-01-17 23:35:49 +0530

    added link of admin

--- src/component/products/footer/index.js
+++ src/component/products/footer/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -10,7 +11,9 @@ const Footer = () => {
       </p>
       <small>
         Copyright @Pragya RO System 2022 <br />
-        All Rights Reserved.
+        <Link to="/admin" style={{ color: "black", textDecoration: "none" }}>
+          All Rights Reserved.
+        </Link>
       </small>
     </footer>
   );
